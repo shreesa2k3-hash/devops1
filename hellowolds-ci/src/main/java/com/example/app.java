2479a9a98b3bf829b4e16ff437a41feb4fa0bb92
Punya Shree <your-email@example.com>
2026-04-01 12:37:56 +0530

    first commit

--- hellowolds-ci/src/main/java/com/example/app.java
+++ hellowolds-ci/src/main/java/com/example/app.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class app {
 	public static void main(String[] args) {
-		System.out.println("Hello, Continuous Integration!");
+		System.out.println("Hello, Punya");
 		}
 }
